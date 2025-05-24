76a3c8d21e739eb0a69eab5fb1a4c6a2b49d8517
Talles <talves2503@gmail.com>
2025-05-23 23:14:20 -0300

    docs: login route swagger

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -43,6 +43,48 @@ const userController = new UserController();
  */
 userRouter.post("/register", userController.createUser);
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Realiza o login do usuário
+ *     tags: [Usuários]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: joao@exemplo.com
+ *               password:
+ *                 type: string
+ *                 example: senha123
+ *     responses:
+ *       200:
+ *         description: Login realizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                      id:
+ *                        type: string
+ *                        description: Id do usuário
+ *       401:
+ *         description: Credenciais inválidas
+ *       404:
+ *         description: Usuário não encontrado
+ *       422:
+ *         description: Dados de login inválidos
+ *       500:
+ *         description: Erro interno do servidor
+ */
 userRouter.post("/login", userController.loginUser)
 
 export default userRouter;
