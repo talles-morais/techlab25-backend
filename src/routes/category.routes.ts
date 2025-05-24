3943c03ab51e7b04ea567fc37da5cd5122aceb10
Talles <talves2503@gmail.com>
2025-05-24 01:13:12 -0300

    docs: create category swagger doc

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -5,6 +5,32 @@ import { CategoryController } from "../controllers/category.controller";
 const categoriesRouter = Router();
 const categoryController = new CategoryController();
 
+/**
+ * @swagger
+ * /categories/create:
+ *   post:
+ *     summary: Cria uma nova categoria
+ *     description: Utiliza middleware de autenticação.
+ *     tags:
+ *       - Categorias
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Eletrônicos"
+ *     responses:
+ *       201:
+ *         description: Categoria criada com sucesso
+ *       401:
+ *         description: Não autorizado
+ *       422:
+ *         description: Dados inválidos
+ */
 categoriesRouter.post(
   "/create",
   authMiddleware(),
